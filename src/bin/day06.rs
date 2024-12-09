913816bc802d8e2c73793676381be6090319ddec
Mark Rutten <42272613+mgrutten@users.noreply.github.com>
2024-12-09 21:12:02 +1030

    Day 6.

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,7 +1,7 @@
 use array2d::Array2D;
+use std::collections::HashSet;
 use std::error::Error;
 use std::fs;
-
 
 pub fn print_map(map: &Array2D<char>) {
     for row in map.rows_iter() {
@@ -23,9 +23,10 @@ pub fn find_guard(map: &Array2D<char>) -> (i32, i32) {
     panic!("No guard found");
 }
 
-pub fn move_guard(map: &mut Array2D<char>,
+pub fn move_guard(map: &Array2D<char>,
                   location: &(i32, i32),
-                  step: &(i32, i32)) -> Option<(i32, i32)> {
+                  step: &(i32, i32),
+                  visited: &mut HashSet<(i32, i32)>) -> Option<(i32, i32)> {
     let mut new_location = location.clone();
     loop {
         let next_row = new_location.0 + step.0;
@@ -37,11 +38,68 @@ pub fn move_guard(map: &mut Array2D<char>,
         if map[(next_row as usize, next_col as usize)] == '#' {
             break;
         }
-        map[(next_row as usize, next_col as usize)] = 'X';
         new_location = (next_row, next_col);
+        visited.insert(new_location);
     }
 
     Some(new_location)
+}
+
+
+pub enum CycleResult {
+    OutOfBounds,
+    NewLocation((i32, i32)),
+    Cycle,
+}
+
+
+pub fn move_guard_with_step(map: &Array2D<char>,
+                            location: &(i32, i32),
+                            step: &(i32, i32),
+                            visited: &mut HashSet<((i32, i32), (i32, i32))>) -> CycleResult {
+    let mut new_location = location.clone();
+    loop {
+        let next_row = new_location.0 + step.0;
+        let next_col = new_location.1 + step.1;
+        if next_row < 0 || next_row >= map.num_rows() as i32 ||
+            next_col < 0 || next_col >= map.num_columns() as i32 {
+            return CycleResult::OutOfBounds;
+        }
+        if map[(next_row as usize, next_col as usize)] == '#' {
+            break;
+        }
+        new_location = (next_row, next_col);
+        if visited.contains(&(new_location, *step)) {
+            return CycleResult::Cycle;
+        }
+        visited.insert((new_location, *step));
+    }
+
+    CycleResult::NewLocation(new_location)
+}
+
+
+pub fn test_for_cycle(map: &Array2D<char>,
+                      initial_location: &(i32, i32)) -> bool {
+    // Initial location and direction
+    let mut location = initial_location.clone();
+    let mut step = (-1, 0);
+
+    // Set of visited locations
+    let mut visited = HashSet::new();
+    visited.insert((location, step));
+
+    // Keep moving and turning right
+    loop {
+        match move_guard_with_step(&map, &location, &step, &mut visited) {
+            CycleResult::Cycle => return true,
+            CycleResult::NewLocation(value) => location = value,
+            CycleResult::OutOfBounds => break,
+        }
+        step = turn_right(&step);
+    }
+
+    false
 }
 
 pub fn turn_right(step: &(i32, i32)) -> (i32, i32) {
@@ -58,27 +116,59 @@ pub fn turn_right(step: &(i32, i32)) -> (i32, i32) {
     }
 }
 
-pub fn part1(orig_map: &Array2D<char>) {
-    let mut map = orig_map.clone();
 
-    let mut location = find_guard(&map);
-    map[(location.0 as usize, location.1 as usize)] = 'X';
+pub fn find_locations(map: &Array2D<char>,
+                      initial_location: &(i32, i32)) -> HashSet<(i32, i32)> {
+    // Initial location
+    let mut location = initial_location.clone();
+
+    // Set of visited locations
+    let mut visited = HashSet::new();
+    visited.insert(location);
 
     // Keep moving and turning right
     let mut step = (-1, 0);
     loop {
-        match move_guard(&mut map, &location, &step) {
+        match move_guard(&map, &location, &step, &mut visited) {
             Some(value) => location = value,
             None => break,
         }
         step = turn_right(&step);
     }
 
-    // Count the number of 'X'
-    let visits: i32 = map.elements_row_major_iter()
-        .map(|e| if *e == 'X' { 1 } else { 0 })
-        .sum();
-    println!("Part 1: {}", visits);
+    visited
+}
+
+pub fn part1(map: &Array2D<char>) {
+    // Initial location
+    let initial_location = find_guard(&map);
+
+    // Set of visited locations
+    let visited = find_locations(&map, &initial_location);
+
+    // The number of visited locations
+    println!("Part 1: {}", visited.len());
+}
+
+pub fn part2(map: &Array2D<char>) {
+    // Initial location
+    let initial_location = find_guard(&map);
+
+    // Set of visited locations
+    let visited = find_locations(&map, &initial_location);
+
+    // Try each original visited location for new obstacle location
+    let mut cycle_count = 0;
+    for (row, col) in visited {
+        if (row, col) != initial_location {
+            let mut test_map = map.clone();
+            test_map[(row as usize, col as usize)] = '#';
+            if test_for_cycle(&test_map, &initial_location) { cycle_count += 1 }
+        }
+    }
+
+    // The number locations that result in a cycle
+    println!("Part 2: {}", cycle_count);
 }
 
 
@@ -94,6 +184,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let map = Array2D::from_rows(&map_vec).unwrap();
 
     part1(&map);
+    part2(&map);
 
     Ok(())
 }
