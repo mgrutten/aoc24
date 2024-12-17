e0aa31c0681b8abb9765552ed38b0753a7bdc658
Mark Rutten <42272613+mgrutten@users.noreply.github.com>
2024-12-17 18:01:28 +1030

    Day 17.

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::error::Error;
 use std::fs;
 
@@ -16,10 +17,10 @@ fn combo(operand: u8,
 }
 
 
-fn part1(register_a: u64,
-         register_b: u64,
-         register_c: u64,
-         instructions: &Vec<[u8; 2]>) {
+fn run_program(register_a: u64,
+               register_b: u64,
+               register_c: u64,
+               instructions: &Vec<[u8; 2]>) -> (u64, u64, u64, Vec<u8>) {
     let mut reg_a = register_a;
     let mut reg_b = register_b;
     let mut reg_c = register_c;
@@ -50,7 +51,7 @@ fn part1(register_a: u64,
             4 => reg_b = reg_b ^ reg_c,
             // out
             5 => {
-                output.push((combo_operand % 8).to_string());
+                output.push((combo_operand % 8) as u8);
             }
             // bdv
             6 => reg_b = reg_a / 2_u64.pow(combo_operand as u32),
@@ -63,11 +64,60 @@ fn part1(register_a: u64,
         if pointer >= instructions.len() {
             break;
         }
-        
     }
 
-    println!("{}", output.join(","));
+    (reg_a, reg_b, reg_c, output)
 }
+
+
+fn part1(register_a: u64,
+         register_b: u64,
+         register_c: u64,
+         instructions: &Vec<[u8; 2]>) {
+    let output = run_program(register_a, register_b, register_c, instructions);
+    let string = output.3.iter()
+        .map(|b| b.to_string())
+        .collect::<Vec<String>>()
+        .join(",");
+
+    println!("Part 1: {}", string);
+}
+
+fn part2(register_b: u64,
+         register_c: u64,
+         instructions: &Vec<[u8; 2]>) {
+    let expected = instructions.iter()
+        .map(|instruction| instruction.clone().into_iter())
+        .flatten()
+        .collect::<Vec<u8>>();
+
+    // Run the program until the last jump
+    // Then the last instruction prints register B mod 8
+    // Find values that give the register B that we want for each digit.
+    // Multiply possible reg A values by 8 at each iteration and check 0..8 around those
+    let all_but_last = instructions[..instructions.len() - 2].to_vec();
+
+    let mut valid_reg = HashSet::<u64>::new();
+    valid_reg.insert(0);
+    for output_index in (0..expected.len()).rev() {
+        let mut new_valid = HashSet::new();
+        for reg in valid_reg.iter() {
+            for i in 0..8 {
+                let output = run_program(reg + i, register_b, register_c, &all_but_last);
+                let reg_b = (output.1 % 8) as u8;
+
+                if reg_b == expected[output_index] {
+                    new_valid.insert(8 * (reg + i));
+                }
+            }
+        }
+        valid_reg = new_valid;
+    }
+
+    let min_reg = valid_reg.iter().min().unwrap() / 8;
+    println!("Part 2: {}", min_reg);
+}
+
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Read in example
@@ -88,6 +138,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .collect::<Vec<_>>();
 
     part1(register_a, register_b, register_c, &instructions);
+    part2(register_b, register_c, &instructions);
 
     Ok(())
 }
