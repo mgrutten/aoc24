28ab2fb2ba8d18ee2eed7d92edc2eaf9abfb31c6
Mark Rutten <42272613+mgrutten@users.noreply.github.com>
2024-12-20 17:18:40 +1030

    Day 20.

--- src/bin/day20.rs
+++ src/bin/day20.rs
@@ -1,6 +1,6 @@
 use array2d::Array2D;
 use std::error::Error;
-use std::{fmt, fs};
+use std::fs;
 
 #[derive(Debug, Clone, PartialEq)]
 enum MapContent {
@@ -8,9 +8,9 @@ enum MapContent {
     Empty,
     Start,
     End,
-    Visited,
 }
 
+/*
 impl fmt::Display for MapContent {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
@@ -33,6 +33,8 @@ fn print_map(map: &Array2D<MapContent>) {
     }
 }
 
+ */
+
 
 fn find_start(map: &Array2D<MapContent>) -> (usize, usize) {
     map.enumerate_row_major()
@@ -53,11 +55,6 @@ fn initial_path(map: &Array2D<MapContent>) -> Vec<(usize, usize)> {
             let new_row = location.0 as i32 + direction.0;
             let new_col = location.1 as i32 + direction.1;
 
-            if new_row < 0 || new_col < 0 ||
-                new_row >= map.num_rows() as i32 || new_col >= map.num_columns() as i32 {
-                continue;
-            }
-
             let new_location = (new_row as usize, new_col as usize);
             if new_location == previous {
                 continue;
@@ -76,38 +73,24 @@ fn initial_path(map: &Array2D<MapContent>) -> Vec<(usize, usize)> {
 }
 
 
-fn find_shortcuts(path: &Vec<(usize, usize)>) {
-    //let mut cuts = HashMap::new();
+fn find_shortcuts(path: &Vec<(usize, usize)>, threshold: i32, shortcut: i32) {
     let mut count: usize = 0;
     for idx1 in 0..path.len() {
         for idx2 in idx1 + 1..path.len() {
             let l1 = path[idx1];
             let l2 = path[idx2];
-            let row_diff = (l1.0 as i32 - l2.0 as i32).abs();
-            let col_diff = (l1.1 as i32 - l2.1 as i32).abs();
+            let diff = (l1.0 as i32 - l2.0 as i32).abs() + (l1.1 as i32 - l2.1 as i32).abs();
 
-            if (row_diff <= 2 && col_diff == 0) || (row_diff == 0 && col_diff <= 2) {
-                //println!("{}, {}", row_diff, col_diff);
-
-                let dist = idx2 - idx1;
-                if dist >= 102 {
+            if diff <= shortcut {
+                let dist = (idx2 - idx1) as i32 - diff;
+                if dist >= threshold {
                     count += 1;
-                    //*cuts.entry(dist - 2).or_insert(0) += 1;
                 }
             }
         }
     }
 
     println!("count: {}", count);
-
-    /*
-    let mut keys = cuts.keys().collect::<Vec<_>>();
-    keys.sort();
-    for key in keys {
-        println!("{}: {}", key, cuts.get(key).unwrap());
-    }
-
-     */
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -128,13 +111,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         .collect::<Vec<_>>();
 
     let map = Array2D::from_rows(&map_vec).unwrap();
-    //print_map(&map);
 
     let path = initial_path(&map);
-    //println!("path: {:?}", path);
 
-    find_shortcuts(&path);
-    //println!("{}", count);
+    find_shortcuts(&path, 100, 2);
+    find_shortcuts(&path, 100, 20);
 
     Ok(())
 }
