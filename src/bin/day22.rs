b8aef8759bdd2be7674f41ee7e712ea72d1e23c7
Mark Rutten <42272613+mgrutten@users.noreply.github.com>
2024-12-22 21:34:18 +1030

    Day 22.

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs;
 
@@ -16,7 +17,6 @@ fn part1(secrets: &Vec<u64>) {
         let mut num = *secret;
         for _ in 0..2000 {
             num = gen_next(num);
-            //println!("{}", num);
         }
         total += num;
     }
@@ -24,17 +24,46 @@ fn part1(secrets: &Vec<u64>) {
 }
 
 
+fn part2(secrets: &Vec<u64>) {
+    let mut patterns = HashMap::new();
+
+    for secret in secrets {
+        let mut digits = Vec::new();
+        let mut num = *secret;
+        digits.push((num % 10) as i8);
+        for _ in 0..2000 {
+            num = gen_next(num);
+            digits.push((num % 10) as i8);
+        }
+        let diffs = digits.windows(2).map(|w| w[1] - w[0]).collect::<Vec<_>>();
+
+        let mut unique = HashSet::new();
+        for idx in 0..=diffs.len() - 4 {
+            let seq = &diffs[idx..idx + 4];
+            if unique.insert(seq.to_vec()) {
+                *patterns.entry(seq.to_vec()).or_insert(0) += digits[idx + 4] as u64;
+            }
+        }
+    }
+
+    let max_pattern = patterns.iter()
+        .max_by_key(|&(_, count)| count).unwrap();
+
+    println!("Part 2: {:?}", max_pattern);
+}
+
+
 fn main() -> Result<(), Box<dyn Error>> {
 
     // Read in example
     let file_str: String = fs::read_to_string("data/day22/day22.txt")?;
-
-
+    
     let secrets = file_str.lines()
         .map(|line| line.parse::<u64>().unwrap())
         .collect::<Vec<_>>();
 
     part1(&secrets);
-    
+    part2(&secrets);
+
     Ok(())
 }
